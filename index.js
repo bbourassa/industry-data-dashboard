9357d50f45e1f2c888022094cbaa3a380e301dd9
bbourassa <bnboura@cs.unc.edu>
2023-10-13 09:54:21 -0400

    fixing cors issue

--- index.js
+++ index.js
@@ -19,7 +19,16 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: false
 }));
-app.use(cors());
+// app.use(cors());
+
+const cors=require("cors");
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions))
 
 app.use(allowCrossDomain);
 
