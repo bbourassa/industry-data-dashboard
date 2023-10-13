3e30d0642ba8b4fad03e6f9c29ff70831c864020
bbourassa <bnboura@cs.unc.edu>
2023-10-13 09:47:29 -0400

    updated api key

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ let express = require('express'),
  const industryRoutes = require('./industry.routes');
 
 const allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,PUT,POST,DELETE.PATCH');
+    res.header('Access-Control-Allow-Origin', 'https://industry-data-dashboard-70fab67995be.herokuapp.com/');
+    res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization');
     next();
 }
